893190023765fcd064e6cd711cf5648ca9a4fd3e
Elena <agaphonova.ea@gmail.com>
2021-08-28 23:34:27 +0300

    декоративные элементы каталога,доработка логотипа

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -1,3 +1,18 @@
+// var navMain = document.querySelector('.main-nav');
+// var navToggle = document.querySelector('.main-nav__toggle');
+
+// navMain.classList.remove('main-nav--nojs');
+
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -10,5 +25,19 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+var navColor = document.querySelector('.page-header__line-menu');
+var Toggle = document.querySelector('.main-nav__toggle');
+
+Toggle.addEventListener('click', function() {
+  if (navColor.classList.contains('page-header__line-menu--closed')) {
+    navColor.classList.remove('page-header__line-menu--closed');
+    navColor.classList.add('page-header__line-menu--opened');
+  } else {
+    navColor.classList.add('page-header__line-menu--closed');
+    navColor.classList.remove('page-header__line-menu--opened');
   }
 });

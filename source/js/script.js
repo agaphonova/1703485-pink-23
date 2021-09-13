352cfca5299e9a557ab3fd1fe3ced5d68b93a62b
Elena <agaphonova.ea@gmail.com>
2021-09-14 01:31:29 +0300

    preloading fonts, переверстка меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,22 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+
 var navColor = document.querySelector('.page-header__line-menu');
-var Toggle = document.querySelector('.menu-btn');
+var Toggle = document.querySelector('.main-nav__toggle');
 
 Toggle.addEventListener('click', function() {
   if (navColor.classList.contains('page-header__line-menu--closed')) {
@@ -50,21 +67,6 @@ Toggle.addEventListener('click', function() {
 //     slides[slideIndex - 1].style.display = "block";
 //     dots[slideIndex - 1].className += " active";
 // }
-
-// var navMain = document.querySelector('.main-nav');
-// var navToggle = document.querySelector('.main-nav__toggle');
-
-// navMain.classList.remove('main-nav--nojs');
-
-// navToggle.addEventListener('click', function() {
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
 
 
 
